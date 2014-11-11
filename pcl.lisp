8ab284d25c7b787b34630de85b27225d339ae85c
liuenze <liu.enze@me.com>
2014-11-11 23:07:07 +0800

    add with-gensyms and once-only macro in chapter8

--- pcl.lisp
+++ pcl.lisp
@@ -41,3 +41,15 @@
        (loop repeat (funcall fn index) do
             (format t "*"))
        (format t "~%")))
+
+;;;chapter 8
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defmacro once-only ((&rest names) &body body)
+  (let ((gensyms (loop for n in names collect (gensym))))
+    `(let (,@(loop for g in gensyms collect `(,g (gensym))))
+       `(let (,,@(loop for g in gensyms for n in names collect ``(,,g ,,n)))
+          ,(let (,@(loop for n in names for g in gensyms collect `(,n ,g)))
+                ,@body)))))
