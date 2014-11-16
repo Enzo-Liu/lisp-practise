553f4a23e745470a4844ce977d6d02d9f75bb40e
liuenze <liu.enze@me.com>
2014-11-16 11:45:16 +0800

    add sicp lecture4b <pattern match and evluate>

---
+++ pattern-match.lisp
@@ -0,0 +1,196 @@
+;;; this program is for sicp lecture 4b
+;;; it describes a common method to do pattern matching and calculate
+;;; lang common lisp
+
+
+(defconstant +FAILED+ 'failed)
+
+(defun atomp (x)
+  (and (not (consp x))
+       (not (null x))))
+
+(defvar *init-dict* nil)
+
+(defun variable-name (pattern)
+  (cadr pattern))
+
+(defun extend-dict (pattern exp dict)
+  (let* ((name (variable-name pattern))
+         (v (assoc name dict)))
+    (cond ((not v) (cons (list name exp) dict))
+          ((eq (cadr v) exp) dict)
+          (t +Failed+))))
+
+(equal (extend-dict '(? x) '10 *init-dict*)     '((x 10)))
+(equal (extend-dict '(? x) '10 '((x 10)))      '((x 10)))
+(equal (extend-dict '(? x) '10 '((x 11)))      +FAILED+)
+
+(defun lookup (var dict)
+  (let ((v (assoc var dict)))
+    (if (not v)
+        var
+        (cadr v))))
+
+(equal (assoc 'x '((x 10)))         '(x 10))
+(equal (assoc 'x '((y 10) (x 10)))  '(x 10))
+(equal (assoc 'z '((y 10) (x 10)))   nil)
+
+
+(equal (lookup 'x '((x 10)))       10)
+(equal (lookup 'y '((x 10)))       'y)
+
+(defun arbitrary-constant-p (pattern)
+  (and (consp pattern) (eq (car pattern) '?c)))
+
+(defun constant-p (pattern)
+  (numberp pattern))
+
+(defun arbitrary-variable-p (pattern)
+  (and (consp pattern) (eq (car pattern) '?v)))
+
+(defun variable-p (pattern)
+  (symbolp pattern))
+
+(defun arbitrary-expression-p (pattern)
+  (and (consp pattern) (eq (car pattern) '?)))
+
+(defun match (pattern exp dict)
+  (cond
+    ((and (null pattern) (null exp)) dict)
+    ((eq dict +FAILED+) +FAILED+)
+    ((atomp pattern)
+     (if (atomp exp)
+         (if (eq pattern exp)
+             dict
+             +FAILED+)))
+    ((arbitrary-constant-p pattern)
+     (if (constantp exp)
+         (extend-dict pattern exp dict)
+         +FAILED+))
+    ((arbitrary-variable-p pattern)
+     (if (variable-p exp)
+         (extend-dict pattern exp dict)
+         +FAILED+))
+    ((arbitrary-expression-p pattern)
+     (extend-dict pattern exp dict))
+    ((atomp exp) +FAILED+)
+    (t (match (cdr pattern) (cdr exp)
+              (match (car pattern) (car exp) dict)))))
+
+(equal (match nil nil *init-dict*)                         *init-dict*)
+(equal (match nil nil '((x 10)))                         '((x 10)))
+(equal (match '* '* *init-dict*)                             *init-dict*)
+
+(equal (match '(? x) '10 *init-dict*)                       '((x 10)))
+(equal (match '(?c x) '10 *init-dict*)                      '((x 10)))
+(equal (match '(?v x) '10 *init-dict*)                       +FAILED+)
+(equal (match '(?v x) 'a *init-dict*)                       '((x a)))
+
+(equal (match '((? x) (? x)) '(10 10) *init-dict*)          '((x 10)))
+(equal (match '((? x) (? x)) '(10 11) *init-dict*)           +FAILED+)
+(equal (match '((? x) (? y)) '(10 11) *init-dict*)          '((y 11) (x 10)))
+
+(defvar *mult-deriv-pattern* '(+ (* (? x) (? y)) (? y)))
+
+(equal (match *mult-deriv-pattern* '(+ (* 3 x) x) nil)      '((y x) (x 3)))
+(equal (match *mult-deriv-pattern* '(+ (* 3 x) 4) nil)       +FAILED+)
+
+(defun skeleton-evaluation-p (exp)
+  (and (consp exp) (eq (car exp) '%)))
+
+(defun eval-exp (skeleton)
+  (cadr skeleton))
+
+
+(defun evaluate (form dict)
+  (if (atomp form)
+      (lookup form dict)
+      (apply (symbol-function (lookup (car form) dict) )
+             (mapcar (lambda (v) (lookup v dict)) (cdr form)))))
+
+(equal (evaluate '(+ x x) '((x 3))) 6)
+
+(defun instantiate (skeleton dict)
+  (labels ((run (s) (cond
+                      ((null s) nil)
+                      ((atomp s) s)
+                      ((skeleton-evaluation-p s)
+                       (evaluate (eval-exp s) dict))
+                      (t (cons (run (car s)) (run (cdr s)))))))
+    (run skeleton)))
+
+(equal (instantiate 'foo *init-dict*)           'foo)
+(equal (instantiate '(f a b) *init-dict*)       '(f a b))
+
+
+(defvar *dict1* (match *mult-deriv-pattern* '(+ (* 3 x) x) nil))
+
+(equal (instantiate '(% y) *dict1*)              'x)
+(equal (instantiate '(% x) *dict1*)              '3)
+(equal (instantiate '(+ (% x) (% y)) *dict1*)    '(+ 3 x))
+(equal (instantiate '(% (+ x x)) *dict1*)        6)
+
+(defun pattern (rule)
+  (car rule))
+(defun skeleton (rule)
+  (cadr rule))
+
+(defun simplifier (the-rules)
+  (labels ((try-rules (exp rules)
+             (if (null rules)
+                 exp
+                 (let ((dict (match (pattern (car rules))
+                               exp
+                               *init-dict*)))
+                   (if (eq dict +FAILED+)
+                       (try-rules exp (cdr rules))
+                       (simplify-exp
+                        (instantiate
+                         (skeleton (car rules)) dict))))))
+           (simplify-exp (exp)
+             (try-rules (if (consp exp)
+                            (mapcar #'simplify-exp exp)
+                            exp)
+                        the-rules)))
+    #'simplify-exp))
+
+(defparameter *deriv-rules*
+  '(
+    ((dd (?c c) (? v))             0)
+    ((dd (?v v) (? v))             1)
+    ((dd (?v u) (? v))             0)
+    ((dd (+ (? x1) (? x2)) (? v))  (+ (dd (% x1) (% v))
+                                    (dd (% x2) (% v))))
+    ((dd (* (? x1) (? x2)) (? v))  (+ (* (% x1) (dd (% x2) (% v)))
+                                    (* (dd (% x1) (% v)) (% x2))))
+    ((dd (** (? x) (?c n)) (? v))  (* (* (% n) (+ (% x) (% (- n 1))))
+                                    (dd (% x) (% v))))
+    ))
+
+
+(defparameter *algebra-rules*
+  '(
+    (((? op) (?c c1) (?c c2))                (% (op c1 c2)))
+    (((? op) (?  e ) (?c c))                 ((% op) (% c) (% e)))
+    ((+ 0 (? e))                             (% e))
+    ((* 1 (? e))                             (% e))
+    ((* 0 (? e))                             0)
+    ((* (?c c1) (* (?c c2) (? e )))          (* (% (* c1 c2)) (% e)))
+    ((* (?  e1) (* (?c c ) (? e2)))          (* (% c ) (* (% e1) (% e2))))
+    ((* (* (? e1) (? e2)) (? e3))            (* (% e1) (* (% e2) (% e3))))
+    ((+ (?c c1) (+ (?c c2) (? e )))          (+ (% (+ c1 c2)) (% e)))
+    ((+ (?  e1) (+ (?c c ) (? e2)))          (+ (% c ) (+ (% e1) (% e2))))
+    ((+ (+ (? e1) (? e2)) (? e3))            (+ (% e1) (+ (% e2) (% e3))))
+    ((+ (* (?c c1) (? e)) (* (?c c2) (? e))) (* (% (+ c1 c2)) (% e)))
+    ((* (? e1) (+ (? e2) (? e3)))            (+ (* (% e1) (% e2))))
+    ))
+
+(defun dsimp (exp) (funcall (simplifier *deriv-rules*) exp))
+(defun alsimp (exp) (funcall (simplifier *algebra-rules*) exp))
+
+(defun deriv (exp)
+  (alsimp (dsimp exp)))
+
+(equal (deriv '(dd (+ x y) x))          1)
+(equal (deriv '(dd (* x y) x))         'y)
+(equal (deriv '(dd (+ (* x y) x) x))   '(+ 1 y))
